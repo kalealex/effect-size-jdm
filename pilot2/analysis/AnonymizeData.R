6249f34fdfd83b873425abc7ba513c17a6692a94
Alex Kale <kalea@uw.edu>
2019-07-22 10:42:08 -0700

    ready for pilot

---
+++ pilot2/analysis/AnonymizeData.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+library(digest)
+
+# function to anonymize worker ids
+anonymize <- function(x, algo="crc32"){
+  unq_hashes <- vapply(unique(x), function(object) digest(object, algo=algo), FUN.VALUE="", USE.NAMES=TRUE)
+  unname(unq_hashes[x])
+}
+
+# load raw data
+raw_df <- read_csv("raw_data/pilot.csv")
+
+# anonymize worker ids
+anonymous_df <- raw_df %>%
+  mutate(workerId=anonymize(workerId))
+
+# save file
+write.csv(anonymous_df, file = "pilot-anonymous.csv", row.names=FALSE)
